c97b3e5fb98dc68481d297e3d8345cb8eae7e91c
MichielJanssensWork <36403557+MichielJanssensWork@users.noreply.github.com>
2018-11-03 18:29:17 +0100

    Added css from backup

--- src/router.js
+++ src/router.js
@@ -14,12 +14,17 @@ export default new Router({
       component: Home
     },
     {
+      path: '/puppet',
+      name: 'Puppet',
+      component: () => import(/* webpackChunkName: "about" */ './views/Puppet.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="background">
+
+
+    <div class="wood">
+
+    </div>
+    <div class="drapes-top">
+
+    </div>
+
+    <div class="menu-block">
+      <img class="logo" src="../../../turbotheatre_BAK/src/assets/img/turbo-theatre-logo.svg">
+      <ul>
+        <li><router-link to="/puppet" class="button">Start session</router-link></li>
+        <li><router-link to="/puppet" class="button">Puppet list</router-link></li>
+        <li><router-link to="/puppet" class="button">Options</router-link></li>
+        <li><router-link to="/puppet" class="button">Credits</router-link></li>
+      </ul>
+    </div>
+
+    <div class="container">
+      <!--This is the artboard for the puppets-->
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data () {
+    return {
+      msg: 'I am the home page'
+    }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
